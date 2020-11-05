f8ac11fa36b7e4b03d0548f1af27832a9435b53f
Mathias <mathyvermeiren@hotmail.com>
2020-11-05 17:16:02 +0100

    api calls

--- lib/models/speakers.dart
+++ lib/models/speakers.dart
@@ -5,4 +5,17 @@ class Speaker {
   String intro;
 
   Speaker({this.firstName, this.lastName, this.competence, this.intro});
+
+  Speaker.fromJson(Map<String, dynamic> json)
+      : firstName = json['firstName'],
+        lastName = json['lastName'],
+        competence = json['competence'],
+        intro = json['intro'];
+
+  Map<String, dynamic> toJson() => {
+        'firstName': firstName,
+        'lastName': lastName,
+        'competence': competence,
+        'intro': intro,
+      };
 }
--- lib/services/speakers_service.dart
+++ lib/services/speakers_service.dart
@@ -1,8 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter_opleiding_051120/data/speakers_data.dart';
 import 'package:flutter_opleiding_051120/models/speakers.dart';
+import 'package:http/http.dart' as http;
 
 class SpeakersService {
   Future<List<Speaker>> getSpeakers() async {
-    return await speakersMock;
+    final http.Response response = await http
+        .get('https://flutteropleiding-f66b7.firebaseio.com/speakers.json');
+
+    Map<String, dynamic> fetchedData = json.decode(response.body);
+
+    List<Speaker> speakers = [];
+
+    fetchedData.forEach((String key, value) {
+      Speaker speaker = Speaker.fromJson(value);
+      speakers.add(speaker);
+    });
+
+    return speakers;
+  }
+
+  void populateFireBase() async {
+    var speakers = await speakersMock;
+    final Map<String, String> headers = {"Content-Type": "application/json"};
+    for (Speaker speaker in speakers) {
+      final http.Response response = await http.post(
+          'https://flutteropleiding-f66b7.firebaseio.com/speakers.json',
+          headers: headers,
+          body: json.encode(speaker));
+    }
   }
 }
